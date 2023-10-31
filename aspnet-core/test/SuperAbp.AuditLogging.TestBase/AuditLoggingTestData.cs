1426eb29c383b1ab0bcf9ed0e1a51ccf55da0c79
陈磊 <gbdvjxu1257q@outlook.com>
2023-10-31 14:52:38 +0800

    删除测试

--- aspnet-core/test/SuperAbp.AuditLogging.TestBase/AuditLoggingTestData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using Volo.Abp.DependencyInjection;
-
-namespace SuperAbp.AuditLogging;
-
-public class AuditLoggingTestData : ISingletonDependency
-{
-    public Guid AuditLogId { get; set; } = Guid.NewGuid();
-}
